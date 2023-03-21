db71285c80e74fb43fdb71a0215aceab79adb412
Carvalho <guilhermeclcosta@hotmail.com>
2023-03-20 23:25:34 -0300

    Adicionar arquivos de projeto.

---
+++ Roteiro/Views/ListaView.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace TestDrive.Views
+{
+    public partial class ListaView : ContentPage
+    {
+        public class Veiculo
+        {
+            public string Name { get; set; }
+            public double Price { get; set; }
+            public string FormattedPrice => string.Format("R$ {0}", Price);
+        }
+        public List<Veiculo> Veiculos { get; set; }
+        public ListaView()
+        {
+            InitializeComponent();
+
+            Veiculos = new List<Veiculo>
+            {
+                new Veiculo { Name = "Azera V6", Price = 85000 },
+                new Veiculo { Name = "Onix 1.6", Price = 35000 },
+                new Veiculo { Name = "Fiesta 2.0", Price = 52000 },
+                new Veiculo { Name = "C3 1.0", Price = 22000 },
+                new Veiculo { Name = "Uno Fire", Price = 11000 },
+                new Veiculo { Name = "Sentra 2.0", Price = 53000 },
+                new Veiculo { Name = "Astra Sedan", Price = 39000 },
+                new Veiculo { Name = "Vectra 2.0", Price = 37000 },
+                new Veiculo { Name = "Hilux 4x4", Price = 90000 },
+                new Veiculo { Name = "Montana", Price = 57000 },
+                new Veiculo { Name = "Outlander", Price = 90000 },
+            };
+
+            BindingContext = this;
+        }
+
+        private void listViewVeiculos_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var veiculo = (Veiculo)e.Item;
+
+            Navigation.PushAsync(new DetailView(veiculo));
+        }
+    }
+}
---
+++ Roteiro/Views/SchedulingView.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static TestDrive.Views.ListaView;
+
+namespace TestDrive.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SchedulingView : ContentPage
+	{
+		public Veiculo Veiculo { get; set; }
+
+		public string Name { get; set; }
+		public string Fone { get; set; }
+		public string Email { get; set; }
+
+		DateTime schedulingTime = DateTime.Today;
+        public DateTime SchedulingTime 
+		{
+			get 
+			{
+				return schedulingTime;
+			}
+			set 
+			{
+				schedulingTime = value;
+			} 
+		}
+		public TimeSpan SchedulingHour { get; set; }
+		public SchedulingView(Veiculo veiculo)
+		{
+			InitializeComponent ();
+			this.Veiculo = veiculo;
+			this.BindingContext = this;
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+			DisplayAlert("Agendamento", 
+string.Format(
+@"Nome: {0}
+Telefone: {1}
+Email: {2}
+Data: {3}
+Hora: {4}",
+Name, Fone, Email, SchedulingTime.ToString("dd/MM/yyyy"), SchedulingHour), "OK");
+        }
+    }
+}
