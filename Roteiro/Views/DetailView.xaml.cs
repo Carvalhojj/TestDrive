3557143010ae5a773c796983b825c2858c8a456b
Carvalho <guilhermeclcosta@hotmail.com>
2023-03-21 23:26:19 -0300

    Implementando o padrão MVVM no projeto

--- Roteiro/Views/DetailView.xaml.cs
+++ Roteiro/Views/DetailView.xaml.cs
@@ -1,78 +1,19 @@
 ﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using static TestDrive.Views.ListaView;
+using TestDrive.Models;
+using TestDrive.ViewModels;
 
 namespace TestDrive.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DetailView : ContentPage
     {
-        private const int FREIO_ABS = 800;
-        private const int AR_CONDICIONADO = 1000;
-        private const int MP3_PLAYER = 500;
-
-        public string TextFreioAbs
-        {
-            get { return string.Format("Freio ABS - R$ {0}", FREIO_ABS); }
-        }
-        public string TextArCondicionado
-        {
-            get { return string.Format("Ar Condicionado - R$ {0}", AR_CONDICIONADO); }
-        }
-        public string TextMp3Player
-        {
-            get { return string.Format("MP3 Player - R$ {0}", MP3_PLAYER); }
-        }
-
-        bool freioOn;
-        public bool FreioOn 
-        {
-            get { return freioOn; }
-            set 
-            {
-               freioOn = value;
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(TotalValue));
-            }
-        }
-
-        bool arCondicionadoOn;
-        public bool ArCondicionadoOn
-        {
-            get { return arCondicionadoOn; }
-            set
-            {
-                arCondicionadoOn = value;
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(TotalValue));
-            }
-        }
-
-        bool mp3PlayerOn;
-        public bool Mp3PlayerOn
-        {
-            get { return mp3PlayerOn; }
-            set
-            {
-                mp3PlayerOn = value;
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(TotalValue));
-            }
-        }
-
-        public string TotalValue 
-        { 
-            get 
-            { 
-                return string.Format("Valor Total: R$ {0}", Veiculo.Price + (FreioOn ? FREIO_ABS : 0) + (ArCondicionadoOn ? AR_CONDICIONADO : 0) + (Mp3PlayerOn ? MP3_PLAYER : 0)); 
-            } 
-        }
         public Veiculo Veiculo { get; set; }
         public DetailView(Veiculo veiculo)
         {
             InitializeComponent();
             this.Veiculo = veiculo;
-            this.BindingContext = this;
+            this.BindingContext = new DetailViewModel(veiculo);
         }
 
         private void btProx_Clicked(object sender, System.EventArgs e)
--- Roteiro/Views/SchedulingView.xaml.cs
+++ Roteiro/Views/SchedulingView.xaml.cs
@@ -1,49 +1,38 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using static TestDrive.Views.ListaView;
+using TestDrive.Models;
+using TestDrive.ViewModels;
 
 namespace TestDrive.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class SchedulingView : ContentPage
 	{
-		public Veiculo Veiculo { get; set; }
-
-		public string Name { get; set; }
-		public string Fone { get; set; }
-		public string Email { get; set; }
-
-		DateTime schedulingTime = DateTime.Today;
-        public DateTime SchedulingTime 
-		{
-			get 
-			{
-				return schedulingTime;
-			}
-			set 
-			{
-				schedulingTime = value;
-			} 
-		}
-		public TimeSpan SchedulingHour { get; set; }
+		public SchedulingViewModel ViewModel { get; set; }
 		public SchedulingView(Veiculo veiculo)
 		{
 			InitializeComponent ();
-			this.Veiculo = veiculo;
-			this.BindingContext = this;
+			this.ViewModel = new SchedulingViewModel(veiculo);
+			this.BindingContext = this.ViewModel;
 		}
 
         private void Button_Clicked(object sender, EventArgs e)
         {
 			DisplayAlert("Agendamento", 
-string.Format(
-@"Nome: {0}
-Telefone: {1}
-Email: {2}
-Data: {3}
-Hora: {4}",
-Name, Fone, Email, SchedulingTime.ToString("dd/MM/yyyy"), SchedulingHour), "OK");
+			string.Format(
+			@"
+			Veículo: {0}
+			Nome: {1}
+			Telefone: {2}
+			Email: {3}
+			Data: {4}
+			Hora: {5}",
+			ViewModel.Scheduling.Veiculo.Name, 
+			ViewModel.Scheduling.Name,
+			ViewModel.Scheduling.Fone, 
+			ViewModel.Scheduling.Email, 
+			ViewModel.Scheduling.SchedulingTime.ToString("dd/MM/yyyy"), ViewModel.Scheduling.SchedulingHour), "OK");
         }
     }
 }
